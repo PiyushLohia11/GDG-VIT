36342a39ca3460275937f0587b87575c6031387c
unknown <piyushlohia1106@gmail.com>
2026-01-18 13:31:06 +0530

    defining the function to expand the strings

--- GDG-VIT-25BCE2527.py
+++ GDG-VIT-25BCE2527.py
@@ -1,2 +1,13 @@
 import tkinter as tk
 import turtle
+def expand_string(axiom, rules, iterations):
+    current_string = axiom
+    for _ in range(iterations):
+        next_string = ""
+        for char in current_string:
+            if char in rules:
+                next_string += rules[char]
+            else:
+                next_string += char
+        current_string = next_string
+    return current_string
