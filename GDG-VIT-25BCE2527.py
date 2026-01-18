21af7a9c63064a1aa9885e47c35e5ae9a31aebb4
unknown <piyushlohia1106@gmail.com>
2026-01-18 13:31:52 +0530

    Defining the drawing logic

--- GDG-VIT-25BCE2527.py
+++ GDG-VIT-25BCE2527.py
@@ -11,3 +11,29 @@ def expand_string(axiom, rules, iterations):
                 next_string += char
         current_string = next_string
     return current_string
+
+def draw_l_system(t, instructions, angle, distance):
+    stack = [] # Storage for saving turtle position/heading
+    colors = ["#FF5733", "#C70039", "#900C3F", "#581845", "#1C2833"]
+    color_index = 0
+
+    for char in instructions:
+        if char == 'F':
+            t.forward(distance)
+        elif char == '+':
+            t.right(angle)
+        elif char == '-':
+            t.left(angle)
+        
+        elif char == '[':
+            state = (t.position(), t.heading())
+            stack.append(state)
+        elif char == ']':
+            if stack:
+                position, heading = stack.pop()
+                t.penup()
+                t.goto(position)
+                t.setheading(heading)
+                t.pendown()
+                t.pencolor(colors[color_index % len(colors)])
+                color_index += 1
